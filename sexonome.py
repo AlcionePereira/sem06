fba662195db8b56b5e3c22bc96ce7264d91d1c70
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-10-05 18:39:10 -0300

    Add files via upload

---
+++ sexonome.py
@@ -0,0 +1,12 @@
+nome = str(input().strip())
+sexo = input().strip()
+
+
+
+if sexo == '1':
+    print('Ilmo Sr. {} ' .format(nome))
+
+if sexo =='2' :
+    print('Ilma Sra. {}' .format(nome))
+
+    
